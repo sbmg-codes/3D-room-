2f9987691f693621d0462cfd3c657b05a165eac7
sbmg-codes <tirth.develops@gmail.com>
2026-04-25 16:27:14 +0530

    Improved Renderer

--- src/Renderer.js
+++ src/Renderer.js
@@ -2,12 +2,31 @@ import * as THREE from "three";
 
 export class RendererManager {
   constructor(scene, camera) {
-    this.renderer = new THREE.WebGLRenderer();
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(this.renderer.domElement);
     this.scene = scene;
     this.camera = camera;
     this.zeroMargins();
+    this.renderer.outputColorSpace = THREE.SRGBColorSpace;
+    this.renderer.setPixelRatio(devicePixelRatio);
+    this.renderer.shadowMap.enabled = true;
+    this.renderer.shadowMap.type = THREE.PCFShadowMap;
+    this.renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    this.renderer.toneMappingExposure = 1;
+    this.renderer.physicallyCorrectLights = true;
+
+    window.addEventListener("resize", () => {
+      this.onResize();
+    });
+  }
+
+  onResize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
   }
 
   zeroMargins() {
