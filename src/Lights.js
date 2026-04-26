13e4b6e7886447dce33cfb5bfda6351fbe4e9000
sbmg-codes <tirth.develops@gmail.com>
2026-04-26 19:44:31 +0530

    light work

--- src/Lights.js
+++ src/Lights.js
@@ -8,16 +8,35 @@ import {
 export class LightManager {
   constructor(scene) {
     this.scene = scene;
-    this.ambientLight = new AmbientLight("white", 1);
-    this.ambientLight.castShadow = true;
+    this.lights = {};
     this.threeLightSetup();
   }
 
   threeLightSetup() {
-    const mainLight = new RectAreaLight("white", 3, 10, 10);
+    const mainLight = new RectAreaLight("white", 1, 10, 10);
     mainLight.position.set(0, 10, 0);
     mainLight.lookAt(0, 0, 0);
-    mainLight.position.set;
+    this.lights.main = mainLight;
     this.scene.add(mainLight);
+
+    const secondaryLight = new DirectionalLight("white", 1.2);
+    secondaryLight.position.set(-5, 5, 5);
+    secondaryLight.lookAt(0, 0, 0);
+    this.lights.secondary = secondaryLight;
+    this.scene.add(secondaryLight);
+
+    const accentLight = new PointLight(0xff9966, 2, 20);
+    accentLight.position.set(5, 3, -6);
+    this.lights.accent = accentLight;
+    this.scene.add(accentLight);
+
+    const ambientLight = new AmbientLight("white", 0.4);
+    this.lights.ambient = ambientLight;
+    this.scene.add(ambientLight);
+  }
+
+  configLight(light, position, lookAtPosition) {
+    light.position.set(...position);
+    if (lookAtPosition) light.lookAt(...lookAtPosition);
   }
 }
