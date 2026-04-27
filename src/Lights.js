61050772d4d0cbe05440662faa0b7abda2252d8e
sbmg-codes <tirth.develops@gmail.com>
2026-04-27 10:38:55 +0530

    three point light system

--- src/Lights.js
+++ src/Lights.js
@@ -14,7 +14,7 @@ export class LightManager {
 
   threeLightSetup() {
     const mainLight = new DirectionalLight(0xffff5e6, 2.5);
-    mainLight.position.set(5, 8, 15);
+    mainLight.position.set(5, 8, 5);
     mainLight.castShadow = true;
     mainLight.shadow.mapSize.width = 2048;
     mainLight.shadow.mapSize.height = 2048;
@@ -32,5 +32,18 @@ export class LightManager {
     this.lights.main = mainLight;
 
     this.scene.add(mainLight);
+
+    const fillLight = new DirectionalLight(0xd0e7ff, 0.8);
+    fillLight.position.set(-4, 4, 3);
+    this.scene.add(fillLight);
+
+    const rimLight = new DirectionalLight(0xffd0a0, 0.5);
+    rimLight.position.set(-2, 6, -8);
+    this.lights.rim = rimLight;
+    this.scene.add(rimLight);
+
+    const ambientLight = new AmbientLight(0xfff5e6, 0.3);
+    this.lights.ambient = ambientLight;
+    this.scene.add(ambientLight);
   }
 }
